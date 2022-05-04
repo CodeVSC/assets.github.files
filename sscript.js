c76e54a67b63ca80d127e4c3cd676922f09a9be0
CodeVSC <49163043+CodeVSC@users.noreply.github.com>
2022-05-04 11:18:17 +0200

    Update sscript.js

--- sscript.js
+++ sscript.js
@@ -1,3 +1,5 @@
-var img = document.createElement("img");
-img.src = "https://raw.githubusercontent.com/CodeVSC/assets.github.files/main/leftplayer.png";
-document.body.appendChild(img);
+function sett() {
+    var img = document.createElement("img");
+    img.src = "https://raw.githubusercontent.com/CodeVSC/assets.github.files/main/leftplayer.png";
+    document.body.appendChild(img);
+}
